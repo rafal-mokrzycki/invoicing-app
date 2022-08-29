964acbf616802cfe557432acaf074207cfaacaf9
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-08-29 22:09:13 +0200

    Add count_gross method for calculating +VAT amount

--- invoice.py
+++ invoice.py
@@ -9,3 +9,7 @@ class InvoiceCreator:
         self.invoice_type = invoice_type
         self.issuer_tax_no = issuer_tax_no
         self.recipient_tax_no = recipient_tax_no
+
+
+def count_gross(amount, tax_rate):
+    return amount * tax_rate
