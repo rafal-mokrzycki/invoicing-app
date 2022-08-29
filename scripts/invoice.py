9f32997ffb848eba0615f3c8b9fe0c4df9e2b737
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-08-29 22:49:27 +0200

    Add calculate sum gross, sum net and sum tax. Refactorization

--- scripts/invoice.py
+++ scripts/invoice.py
@@ -8,6 +8,7 @@ import random
 import smtplib
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from config_files.config import config
 
@@ -28,16 +29,24 @@ class InvoiceCreator:
             raise ValueError("Wrong invoice type")
         self.issuer_tax_no = issuer_tax_no
         self.recipient_tax_no = recipient_tax_no
+        # TODO: inner class for position?
         self.positions = positions
         self.prices_net = prices_net
         self.tax_rates = tax_rates
         self.prices_gross = [
-            count_gross(i, j) for i, j in zip(self.prices_net, self.tax_rates)
+            calculate_gross(i, j) for i, j in zip(self.prices_net, self.tax_rates)
         ]
+        self.sum_net = calculate_sum(self.prices_net)
+        self.sum_gross = calculate_sum(self.prices_gross)
+        self.sum_tax = self.sum_gross - self.sum_net
 
 
-def count_gross(amount, tax_rate):
+def calculate_gross(amount, tax_rate):
     if tax_rate in config["TAX_RATES"]:
         return amount + amount * tax_rate
     else:
         raise ValueError("Wrong tax rate")
+
+
+def calculate_sum(iterable):
+    return np.sum(iterable)
