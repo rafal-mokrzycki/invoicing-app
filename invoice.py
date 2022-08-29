d4ab24849b8047652f66cd22a2ad7b8fd486f1b1
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-08-29 22:07:06 +0200

    Add InvoiceCreator class and __init__ method

---
+++ invoice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Utilities to create a new ivoice
+"""
+
+
+class InvoiceCreator:
+    def __init__(self, invoice_type, issuer_tax_no, recipient_tax_no) -> None:
+        self.invoice_type = invoice_type
+        self.issuer_tax_no = issuer_tax_no
+        self.recipient_tax_no = recipient_tax_no
