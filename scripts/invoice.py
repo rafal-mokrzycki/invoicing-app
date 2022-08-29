5ab9f25d39cba731e209a90c81a4b92239612a57
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-08-29 22:44:02 +0200

    Add invoice position and counting gros value for each of them

--- scripts/invoice.py
+++ scripts/invoice.py
@@ -13,13 +13,27 @@ from config_files.config import config
 
 
 class InvoiceCreator:
-    def __init__(self, invoice_type, issuer_tax_no, recipient_tax_no) -> None:
+    def __init__(
+        self,
+        invoice_type,
+        issuer_tax_no,
+        recipient_tax_no,
+        positions,
+        prices_net,
+        tax_rates,
+    ):
         if invoice_type in config["INVOICE_TYPES"]:
             self.invoice_type = invoice_type
         else:
             raise ValueError("Wrong invoice type")
         self.issuer_tax_no = issuer_tax_no
         self.recipient_tax_no = recipient_tax_no
+        self.positions = positions
+        self.prices_net = prices_net
+        self.tax_rates = tax_rates
+        self.prices_gross = [
+            count_gross(i, j) for i, j in zip(self.prices_net, self.tax_rates)
+        ]
 
 
 def count_gross(amount, tax_rate):
