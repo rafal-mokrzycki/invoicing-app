c6fd9e79506ed3acc43158269f0fcd3f4ef5a2f9
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-08-31 12:14:20 +0200

    Add show_invoice

--- scripts/invoice.py
+++ scripts/invoice.py
@@ -29,7 +29,6 @@ class InvoiceCreator:
             raise ValueError("Wrong invoice type")
         self.issuer_tax_no = issuer_tax_no
         self.recipient_tax_no = recipient_tax_no
-        # TODO: inner class for position?
         self.positions = positions
         self.prices_net = prices_net
         self.tax_rates = tax_rates
@@ -39,6 +38,31 @@ class InvoiceCreator:
         self.sum_net = calculate_sum(self.prices_net)
         self.sum_gross = calculate_sum(self.prices_gross)
         self.sum_tax = self.sum_gross - self.sum_net
+
+    def show_invoice(self):
+        print(
+            f"""
+{'='*60}
+{self.invoice_type.upper()} INVOICE
+
+Issuer tax no.: {self.issuer_tax_no}
+Recipient tax no.: {self.recipient_tax_no}
+"""
+        )
+
+        print("Position", "Net amount", "Tax rate", "Gross amount", sep="\t")
+        for i, j, k, l in zip(
+            self.positions, self.prices_net, self.tax_rates, self.prices_gross
+        ):
+            print(i, "\t\t", j, "\t\t", k, "\t\t", l)
+
+        print(
+            f"""
+Net sum: {self.sum_net}
+Tax sum: {self.sum_tax}
+TOTAL SUM: {self.sum_gross}
+{'='*60}"""
+        )
 
 
 def calculate_gross(amount, tax_rate):
