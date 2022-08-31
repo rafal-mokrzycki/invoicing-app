7f5ce51233c8d96641b022cd8222a0e50f5603bd
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-08-31 11:53:21 +0200

    Add tests

--- tests/test_invoice.py
+++ tests/test_invoice.py
@@ -2,8 +2,14 @@
 add --pdb to debug in command line
 use VScode test tools reccomended (the beaker) """
 
-from scripts.invoice import count_gross
+from scripts.invoice import calculate_gross, calculate_sum
 
 
-def test_count_gross():
-    count_gross(1, 0.9)
+def test_calculate_gross():
+    result = calculate_gross(100, 0.23)
+    assert result == 123.0
+
+
+def test_calculate_sum():
+    result = calculate_sum([1, 2, 3])
+    assert result == 6
