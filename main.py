b221eca6cebe982fae61729967247fe9de4b3c57
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-09-09 16:04:04 +0200

    Add current year handling in the copyright section

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 To run type: flask --app hello run
 """
 
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,7 +11,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    current_year = datetime.date.today().year
+    return render_template("index.html", current_year=current_year)
 
 
 if __name__ == "__main__":
