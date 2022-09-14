28b89fb8eedb4cc2234fabcb94b07232d6fff25a
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-09-14 15:52:34 +0200

    Add token generator

--- scripts/persons.py
+++ scripts/persons.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+from curses import get_tabsize
+
 from flask import Flask
 from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from itsdangerous import TimedJSONWebSignatureSerializer
 
 app = Flask(__name__)
 
@@ -23,3 +26,31 @@ class User(db.Model, UserMixin):
 
     def get_id(self):
         return self.email
+
+    def get_token(self, expires_sec=300):
+        serial = TimedJSONWebSignatureSerializer(
+            app.config["SECRET_KEY"], expires_in=expires_sec
+        )
+        return serial.dumps({"email": self.email}).decode("utf-8")
+
+    @staticmethod
+    def verify_token(token):
+        serial = TimedJSONWebSignatureSerializer(app.config["SECRET_KEY"])
+        try:
+            user_email = serial.loads(token)
+        except:
+            None
+        return User.query.get(user_email)
+
+
+class Contractor(db.Model, UserMixin):
+    __tablename__ = "contractors"
+    name = db.Column(db.String(250), nullable=False)
+    surname = db.Column(db.String(250), nullable=False)
+    tax_no = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"""
+{self.name} {self.surname}
+{self.tax_no}
+"""
