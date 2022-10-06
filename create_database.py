1bdd700d2d1090f9dbd404fc24b4776263b42068
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-10-06 20:37:25 +0200

    Update the skeleton of the script

--- create_database.py
+++ create_database.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+def update_credentials():
+    pass
+
+
+def create_database():
+    pass
+
+
+def feed_database():
+    pass
+
+
 if __name__ == "__main__":
     # create credentials.json file with default settings for a DB
-
+    update_credentials()
     # create a database (database.db file with all the required tables, yet empty)
-
+    create_database()
     # feed tables with sample data
-    pass
+    feed_database()
