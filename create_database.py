8c1683514fc28b500c7e8e37c7ccd5d1fe8c2f67
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-10-07 09:41:06 +0200

    Add get_and_check_email) function to get user email and check the regex

--- create_database.py
+++ create_database.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python
-def main():
-    update_credentials()
-    create_database()
-    feed_database()
+import re
+import sys
 
 
-def update_credentials():
-    var = input("Make an input: ")
-    print("update_credentials: {var}")
+def main(email):
+    mail = get_and_check_email(email)
+    return mail
+    # match = None
+    # while match is None:
+    #     email = input("Pass your email: ")
+    #     match = re.fullmatch(
+    #         r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+asd\.[A-Z|a-z]{2,}\b", email
+    #     )
+
+
+def get_and_check_email(email):
+    while True:
+        email = input("Pass your email (or [q] to quit): ")
+        if (
+            re.fullmatch(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", email)
+            is not None
+        ):
+            return email
+        elif email == "q":
+            break
+        else:
+            print("Wrong email format. Try again.")
+            continue
+
+
+def update_credentials(email):
+    print(f"Your email is: {email}")
 
 
 def create_database():
@@ -20,10 +43,14 @@ def feed_database():
 
 if __name__ == "__main__":
     # create credentials.json file with default settings for a DB
-    update_credentials()
+    # update_credentials()
+
     # create a database (database.db file with all the required tables, yet empty)
-    create_database()
+    # create_database()
+
     # feed tables with sample data
-    feed_database()
+    # feed_database()
+
     # final ver
-    main()
+    main(sys.argv)
+    # update_credentials()
