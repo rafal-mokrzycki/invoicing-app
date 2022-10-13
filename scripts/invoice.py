080612958026b0c67178d7a4e5fff0464e3be944
rafal-mokrzycki <rafal.mokrzycki@interia.pl>
2022-10-13 17:18:52 +0200

    Add docstrings

--- scripts/invoice.py
+++ scripts/invoice.py
@@ -4,10 +4,10 @@ Utilities to create a new ivoice
 """
 
 import numpy as np
-from config_files.config import credentials, settings
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from config_files.config import credentials, settings
 from scripts.database import Invoice
 
 app = Flask(__name__)
@@ -20,6 +20,71 @@ CURRENCY = settings["CURRENCY"]
 
 
 class InvoiceForm(Invoice):
+    """
+    A class used to represent an Invoice. Inherits from Invoice.
+
+    Attributes
+    ----------
+    id : int
+        primary key
+
+    amount : float
+        amount of goods in an item
+
+    invoice_no : str
+        invoice number, set automatically
+
+    invoice_type : str
+        invoice type, one of the following: "regular", "advanced payment", "proforma"
+
+    issue_city : str
+        issue city
+
+    issue_date : date
+        issue date
+
+    issuer_tax_no : int
+        issuer tax number
+
+    item : str
+        item / position
+
+    price_net : float
+        price net of a good
+
+    recipient_tax_no : int
+        recipient tax number
+
+    sell_date : date
+        sell date
+
+    sum_gross : float
+        sum gross (sum of all goods in an item * tax rate)
+
+    sum_net : float
+        sum net (sum of all goods in an item)
+
+    tax_rate : float
+        tax rate for an item, one of the following: 0.00, 0.05, 0.08, 0.23
+
+    unit : str
+        unit of an item
+
+    currency : str
+        currency of an item
+
+    issuer_id : int
+        issuer id, foreign key
+
+    recipient_id : int
+        recipient id, foreign key
+
+    Methods
+    ----------
+    __repr__()
+        Returns invoice id
+    """
+
     def __init__(
         self,
         id,
@@ -62,10 +127,12 @@ class InvoiceForm(Invoice):
 
 
 def format_percentages(number):
+    """Formats a floating point number into string, i.e. 0.23 -> '23%'"""
     return str(int(number * 100)) + "%"
 
 
 def format_number(number):
+    """Formats a floating point number into string iwht a currency code, i.e. 2.50 -> '2.50 PLN'"""
     return "{:.2f}".format(number) + f" {CURRENCY}"
 
 
