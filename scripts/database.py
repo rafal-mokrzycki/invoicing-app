f1c73ee450bacd732c889d245d813f767d3702e6
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-11-27 14:23:58 +0100

    ref

--- scripts/database.py
+++ scripts/database.py
@@ -3,7 +3,7 @@ import repackage
 from werkzeug.security import generate_password_hash
 
 repackage.up()
-from app import Base, User, engine
+from app import Base, engine
 
 
 def create_database(drop_all=True):
@@ -17,8 +17,8 @@ def populate_data():
     users = pd.read_csv("config_files/demo_accounts.csv")
     invoices = pd.read_csv("config_files/demo_invoices.csv")
     contractors = pd.read_csv("config_files/demo_contractors.csv")
-    # hashed_password = generate_password_hash(users["password"].iloc[0])
     users["password"] = users["password"].apply(lambda x: generate_password_hash(x))
+
     users.to_sql("accounts", con=engine, index=False, if_exists="replace")
     invoices.to_sql("invoices", con=engine, index=False, if_exists="replace")
     contractors.to_sql("contractors", con=engine, index=False, if_exists="replace")
