41bdf0abfc9593470f3f34e6cd59c4fa41314833
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-01-04 14:56:27 +0100

    add docstrings

--- invoice/formatters.py
+++ invoice/formatters.py
@@ -6,12 +6,31 @@ Format functions
 CURRENCY = "PLN"
 
 
-def format_percentages(number):
-    """Formats a floating point number into string, i.e. 0.23 -> '23%'"""
+def format_percentages(number: float):
+    """Formats a floating point number into string, i.e. 0.23 -> '23%'
+
+    Args:
+        number (float): A float to be formatted.
+
+    Returns:
+        str: Formatted string in format eg. 23%.
+    """
     return str(int(number * 100)) + "%"
 
 
-def format_number(number):
+def format_number(number: float, currency: str = CURRENCY):
     """Formats a floating point number into string wiht a currency code,
-    i.e. 2.50 -> '2.50 PLN'"""
-    return "{:.2f}".format(number) + f" {CURRENCY}"
+    i.e. 2.50 -> '2.50 PLN'
+
+    Args:
+        number (float): A float to be formatted.
+        currency (str, optional): _description_. Defaults to CURRENCY.
+
+    Returns:
+        str: Formatted string in format eg. 2.50 PLN.
+    """
+    return "{:.2f}".format(number) + f" {currency}"
+
+
+def format_bank_account():
+    pass
--- invoice/helpers.py
+++ invoice/helpers.py
@@ -2,16 +2,27 @@
 """
 Helper functions
 """
+import json
+import os
 import time
+from sqlite3 import Connection
 
 import pandas as pd
 import repackage
 from IPython.display import clear_output
 
 
-def append_dict(dict1, dict2):
+def append_dict(dict1: dict, dict2: dict):
     """Appends one dictionary to another one keeping keys from both
-    and returning a sum of their values as strings."""
+    and returning a sum of their values as strings.
+
+    Args:
+        dict1 (dict): Dictionary to append to.
+        dict2 (dict): Dictionary to append.
+
+    Returns:
+        dict: Result dictionary.
+    """
     result = {}
     for i in set(list(dict1.keys()) + list(dict2.keys())):
         if i not in dict1:
@@ -25,8 +36,21 @@ def append_dict(dict1, dict2):
     return result
 
 
-def get_currencies(filename="currencies.csv", columns=None, filepath=None):
-    """Reads the CSV file with currency symbols and parses them to UI."""
+def get_currencies(
+    filename="currencies.csv", columns: list = None, filepath: str = None
+):
+    """Reads the CSV file with currency symbols and parses them to UI.
+
+    Args:
+        filename (str, optional): File where info about currencies is stored.
+        Defaults to "currencies.csv".
+        columns (list, optional): List of columns to use. Defaults to None.
+        filepath (str, optional): Filepath to the file where info about
+        currencies is stored. Defaults to None.
+
+    Returns:
+        list: List of currencies.
+    """
 
     if columns is None:
         columns = ["Currency Code"]
@@ -46,8 +70,20 @@ def get_currencies(filename="currencies.csv", columns=None, filepath=None):
         ]
 
 
-def get_number_of_objects_in_table(database=None, table=None, object=None):
-    """Return number of invoices by type."""
+def get_number_of_objects_in_table(
+    database: Connection = None, table: str = None, object: str = None
+):
+    """Return number of invoices by type.
+
+    Args:
+        database (Connection, optional): Connection to the database.
+        Defaults to None.
+        table (str, optional): Table name. Defaults to None.
+        object (str, optional): Variable name. Defaults to None.
+
+    Returns:
+        dict: Dictionary with invoice names and count of each type.
+    """
 
     query = f"""
     SELECT {object}, count({object})
@@ -58,7 +94,15 @@ def get_number_of_objects_in_table(database=None, table=None, object=None):
     return {elem[0]: elem[1] for elem in list_of_tuples}
 
 
-def wait(step=1, max=3, string="Processing"):
+def wait(step: int = 1, max: int = 3, string: str = "Processing"):
+    """_summary_
+
+    Args:
+        step (int, optional): Length of time between steps. Defaults to 1.
+        max (int, optional): Number of seconds to wait for. Defaults to 3.
+        string (str, optional): String to be displayed in each step.
+        Defaults to "Processing".
+    """
     for x in range(0, max):
         display = string + "." * (x + 1)
         print(display, end="\r")
